511058a2eee4ddea4a28d03ffc40557dec0af3e8
m3122 <3122mackenzie@gmail.com>
2024-11-02 18:08:57 -0300

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-logo">My App</h1>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Services.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Services() {
+  return <h2>Services Page</h2>;
+}
+
+export default Services;
