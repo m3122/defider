f94712c0d2a1cf340724263e7092e5f179f500ef
m3122 <3122mackenzie@gmail.com>
2024-11-13 11:37:18 -0300

    added home features

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import Carousel from '../components/carousel'
+import { Link } from 'react-router-dom';
+import './home.css';
 
 export const HomePage = () => {
   return (
     <>
       <Carousel/>
-      <p>Página de inicio del DEFIDER</p>
+      <div className='parent'>
+        <div className='kin'>
+          <Link to="/gimnasio">
+            <div className='image'>
+              <img
+                src="https://siga.usm.cl/gim/imagen/gim_portada1.jpg"
+                alt="Background"
+              />
+              <div className='text-inside'>
+                Pide hora al gimnasio
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className='kin'>
+          <Link to="/talleres">
+            <div className='image'>
+              <img
+                src="https://usm.cl/wp-content/uploads/2022/03/Expo-DEFIDER-2-300x241.jpg"
+                alt="Background"
+              />
+              <div className='text-inside'>
+                Revisa nuestros talleres
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
